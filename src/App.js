cbf515fd8fe31fa5b1effba7469d7ba88ea61e30
Utsav Bhetwal <utsav_bhetwal2008@yahoo.com>
2022-12-23 13:52:25 -0500

    Update App.js
    remove comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import HeaderComponent from './components/reusableComponents/HeaderComponents';
 import Home from './components/staticComponents/Home';
-//import Project,from './components/staticComponents/Project';
 import Resume from './components/staticComponents/Resume';
 import Contact from './components/staticComponents/Contact';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import FooterComponent from './components/reusableComponents/footerComponents';
 import {Capstone, GWP,ADDA, PM,SDDG, SMWG, WebGIS} from './components/staticComponents/Project'
-// import './App.css';
 class App extends React.Component {
     render() {
         const divStyle = {
-
-
 
         }
         return (
